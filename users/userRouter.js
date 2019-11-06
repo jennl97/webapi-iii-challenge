3a6464e088e5c54e549131683a2c2556a2a759fb
jennl97 <jennhott97@gmail.com>
2019-11-05 22:22:47 -0600

    First commit of webapi III challenge

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,12 @@
-const express = 'express';
+const express = require('express');
+const UserDB = require('./userDb');
+const PostDB = require('../posts/postDb');
+
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
+router.post('/', validateUser, (req, res) => {
+    res.status(200).json(req.users);
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -11,11 +14,20 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-
+    UserDB.get(req.query)
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            message: "Danger Will Robinson!"
+        })
+    })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, (req, res) => {
+    res.status(200).json(req.users);
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -33,10 +45,50 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
+    const {id } = req.params
+    UserDB.getById(id)
+    .then(users => {
+        if(users) {
+            req.users = users;
+            next();
+        } else {
+            res.status(400).json({
+                message: "invalid user id"
+            })
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            message: 'Houston, we have a problem.'
+        });
+    })
 
 };
 
 function validateUser(req, res, next) {
+    const newUser = req.body
+    UserDB.insert(newUser)
+    .then(users => {
+        if(!users.name){
+            res.status(400).json({
+                message: "missing required name field"
+            })
+        } else if (!users){
+            res.status(400).json({
+                message: "missing user data"
+            })
+        } else {
+            next();
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            message: "That's a no from me, Dawg."
+        })
+    })
+
 
 };
 
